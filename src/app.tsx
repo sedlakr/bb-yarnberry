19301fff86f67d489ffc47d6afe46f3a5f179749
Radek Sedlák <r.sedlak@quadient.com>
2023-08-11 09:42:52 +0200

    use hook

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,12 @@ import * as b from "bobril";
 import * as c from "bobwai--color"
 
 function App() {
+	const [clicked, setClicked] = b.useState(0);
 	return <>
-		<div style={{color: c.Chart_Green100}}>Hello world</div>
+		<div style={{color: clicked % 2 === 0 ? c.Warning300 : c.Error300}}>Hello world {clicked}, <button
+			onClick={() => {
+				setClicked(clicked + 1)
+			}}>inc</button></div>
 	</>;
 }
 
