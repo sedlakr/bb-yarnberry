d2f48c8f1df1ef4c13bc8142ddf71b6c288c7860
Radek Sedlák <r.sedlak@quadient.com>
2023-08-11 09:38:00 +0200

    Added bobwai color

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,13 @@
 import * as b from "bobril";
+import * as c from "bobwai--color"
+
+function App() {
+	return <>
+		<div style={{color: c.Chart_Green100}}>Hello world</div>
+	</>;
+}
 
 b.addRoot(() => {
-	return <div>Hello world</div>
+
+	return App()
 }, document.getElementsByTagName("body")[0]);
